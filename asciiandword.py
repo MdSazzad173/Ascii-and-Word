eb4c4138a3f5642b22168cdbfedeb62043f178f7
Md Sazzad Hossen <106554756+MdSazzad173@users.noreply.github.com>
2023-02-18 19:48:08 +0600

    Update asciiandword.py

--- asciiandword.py
+++ asciiandword.py
@@ -1,56 +1,55 @@
-#defining a function that generates ASCCI code
-def ascii_code(line):   
-    lst=[]
+class ascii_and_word:
+    def __init__(self,string):
+        self.string= string 
+#########################################################################################################################  
    
-
-# in this line first we used ord() function for finding the ascii value of the letter and format function used to convert the ascii vlaue into 8bit binary string
-    for i in line:
-        lst.append(format(ord(i),'08b')) 
-        
-
-    result=""
-    for l in lst:
-        result+=l
-    return result
-
-#defining a function that converts ASCII code into the readable sentence
-def asciicode_to_meaning(ascii_value):  
-#creating a dictionary of ascii value and keys  
-    import string
-    ascii_map={}
-    keyboard_chars = list(string.printable)
-    for i in keyboard_chars:
-        ascii_map[format(ord(i),'08b')]=i
-
-    substrings=[]
-    for j in range(0,len(ascii_value),8):       # this loop is for dividing the string 8 bit substring
-        substrings.append(ascii_value[j:j+8])
+    def ascii_code(self):   
+        lst=[]
+        for i in self.string:
+            lst.append(format(ord(i),'08b')) 
+        result=""
+        for l in lst:
+            result+=l
+       
+        return result
+######################################################################################################################   
     
+    def asciimeaning(self):  
+        import string
+        ascii_map={}
+        keyboard_chars = list(string.printable)
+        for i in keyboard_chars:
+            ascii_map[format(ord(i),'08b')]=i
   
-    meaning=''  
-    for a in substrings:                        # comparing the keys
-                                                # and taking the values
-        if a in ascii_map:
-                meaning+=ascii_map[a]           # that contains the 
-                                                # similar keys as the substring
-    return meaning
+        substrings=[]
+        for j in range(0,len(self.string),8):       
+            substrings.append(self.string[j:j+8]) 
+       
+        meaning=''  
+        for a in substrings:                                                                 
+            if a in ascii_map:
+                meaning+=ascii_map[a]                                                   
+        
+        return meaning
+#########################################################################################################################
 
-
-print('1. ASCII to meaning\n 2. Word to ASCII')
-# For mode of oparetion:
-inp=int(input('\nchoose your mode of operation:\t'))
+print('1. Massage to ASCII\n2. ASCII to massage')
+inp=int(input("Enter the mode of operation:\t"))
+##############################################
 
 if inp==1:
-    ascii_value=input('Enter your binary ascii string:\t')
-    print(asciicode_to_meaning(ascii_value))
-
+    ascii_value=input('Enter your massage:\t')
+    obj1=ascii_and_word(ascii_value)
+    print(obj1.ascii_code())
+#########################################################################################################################
 
 elif inp==2:
-    line=input('Enter your line:\t')
-    print(ascii_code(line))
-    
+    ascii_number=input('Enter your ascii code:\t')
+    obj2=ascii_and_word(ascii_number)
+    print(obj2.asciimeaning())
+#########################################################################################################################
 
 else:
-    print('please choose the right option!!!')
-    
-#Completed........
+    print('wrong input!')
+
+##################################################### COMPLETED ##########################################################
